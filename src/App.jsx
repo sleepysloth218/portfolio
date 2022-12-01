bbb3a7e6ff2aceac70579f5ec8e4553d16342409
sleepysloth218 <mehek_jethani+cs1300@brown.edu>
2022-12-01 11:23:45 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./NavBar";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar></NavBar>
+      </div>
+    );
+  }
+}
